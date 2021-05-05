62e5ff27a416887f7e1bd479d4b7622676b3ec9d
unknown <golrale3@gmail.com>
2021-05-05 02:29:36 +0200

    Add some elements on add judgmnet form

--- judgment/forms.py
+++ judgment/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField
+from wtforms import SelectField, StringField, TextAreaField, SubmitField, DateField
+from wtforms.validators import DataRequired
 
 
 class AddJudgments(FlaskForm):
     client_name = SelectField('client_name', choices=[])
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    date = DateField("Date of judgment", format='%Y-%m-%d')
+    submit = SubmitField("Add judgment")
