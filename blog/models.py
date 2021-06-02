692f496b3e78e06ebc2b67734d79c5b3574d6af9
unknown <golrale3@gmail.com>
2021-06-02 11:12:39 +0200

    Date

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Judgment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    date = db.Column(db.DateTime, default=datetime.now)
     client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
 
 
